fa2130a269f24a016c01d8d82bff5c4c94dc85ce
scott-hall7 <scotthall179@gmail.com>
2022-09-12 10:32:48 -0500

    adding add project and add task button

---
+++ dist/main.js
@@ -0,0 +1,37 @@
+const addProjectButton = document.getElementById("add-project-button");
+const addProjectInput = document.getElementById("add-project-input");
+const projectInputButtons = document.querySelectorAll(".project-input-button");
+
+const addTaskButton = document.getElementById("add-task-button");
+const addTaskInput = document.getElementById("add-task-input");
+const taskInputButtons = document.querySelectorAll(".task-input-button");
+
+
+//When Add button is clicked, hide button and display project input.
+addProjectButton.addEventListener('click', () =>    {
+    addProjectButton.style.display = 'none';
+    addProjectInput.style.display = 'flex';
+
+});
+
+//When either button is clicked, hide project input and display button.
+projectInputButtons.forEach(button  =>  {
+    button.addEventListener('click', () => {
+        addProjectButton.style.display = 'flex';
+        addProjectInput.style.display = 'none';
+    });
+});
+
+addTaskButton.addEventListener('click', () =>    {
+    addTaskButton.style.display = 'none';
+    addTaskInput.style.display = 'flex';
+
+});
+
+//When either button is clicked, hide project input and display button.
+taskInputButtons.forEach(button  =>  {
+    button.addEventListener('click', () => {
+        addTaskButton.style.display = 'flex';
+        addTaskInput.style.display = 'none';
+    });
+});
